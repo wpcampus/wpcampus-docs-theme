8bcb2f59ee065b52217327442fd1073cba6f8864
Rachel Cherry <rachel@bamadesigner@com>
2019-01-06 21:03:50 -0500

    Delete the footer in lieu of network footer

--- footer.php
+++ footer.php
@@ -12,10 +12,6 @@
 namespace WP_Rig\WP_Rig;
 
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<?php get_template_part( 'template-parts/footer/info' ); ?>
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
